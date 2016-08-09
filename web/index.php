79444ae7bae12ee4e5b64ed0f867e7d93e2e0e77
Evgeniy Bondarenko <evgeniy.bondarenko@pay-me.ru>
2016-08-09 12:16:54 +0300

    fix dots

--- web/index.php
+++ web/index.php
@@ -1,9 +1,31 @@
 <?php
+
+function replace_dots($arr)
+{
+	$arr2=$arr;
+	foreach($arr as $key=>$val)
+	{
+		if(strpos($key,'.')!==FALSE)
+		{
+			$key2=str_replace('.','_',$key);
+			$arr2[$key2]=$val;
+			unset($arr2[$key]);
+		}
+	}
+	unset($arr);
+	foreach($arr2 as $key=>$val)
+	{
+		if(is_array($val))
+			$arr2[$key]=replace_dots($val);
+	}
+	return $arr2;
+}
 $data=file_get_contents('php://input');
 
 if(!$data)
     die('no data received!');
 $data=json_decode($data,1);
+$data=replace_dots($data);
 if(!$data || !$data['REPORT_ID'])
     die('data deserialization failed!');
 $n=strpos($data['STACK_TRACE'], "\n");
@@ -29,7 +51,7 @@ if(!$config['logDir'])
 	die('Log location not specified!');
 if(($config['logDir'][strlen($config['logDir'])-1])!='/')
 	$config['logDir'].='/';
-$res=file_put_contents($config['logDir'].'mvideo.log',"\n".$data,FILE_APPEND);
+$res=file_put_contents($config['logDir'].'mvideo.log',$data."\n",FILE_APPEND);
 if($res)
   echo 'data saved';
 else
